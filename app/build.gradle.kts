d5cad3edcb93fbb03c0d0658ec7bb3e4e7548705
Fedulaev Maksim <nexpg.my@gmail.com>
2026-04-25 14:47:15 +0300

    Change background color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kernelpanic.vertblock"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2-alpha"
+        versionCode = 3
+        versionName = "0.0.3-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
