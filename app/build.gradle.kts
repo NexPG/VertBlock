b742ec5f6ed2ac58a9ad14e28fb430a263f4d72a
Fedulaev Maksim <nexpg.my@gmail.com>
2026-04-25 10:51:53 +0300

    Update app version to 0.0.2-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kernelpanic.vertblock"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
