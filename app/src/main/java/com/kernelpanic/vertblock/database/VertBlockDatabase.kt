a7f7686f01caf7455fdfd0dfbecfcfdb04d23d07
Fedulaev Maksim <nexpg.my@gmail.com>
2026-05-06 14:19:59 +0300

    Update VertBlockDatabase

--- app/src/main/java/com/kernelpanic/vertblock/database/VertBlockDatabase.kt
+++ app/src/main/java/com/kernelpanic/vertblock/database/VertBlockDatabase.kt
@@ -3,7 +3,12 @@ package com.kernelpanic.vertblock.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [WatchSessionEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [WatchSessionEntity::class, QuizResultEntity::class],
+    version = 2,
+    exportSchema = false
+)
 abstract class VertBlockDatabase : RoomDatabase() {
     abstract fun watchSessionDao(): WatchSessionDao
+    abstract fun quizResultDao(): QuizResultDao
 }
