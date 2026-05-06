e98ee935e06b49078d0ae623f96f3f16812f9b68
Fedulaev Maksim <nexpg.my@gmail.com>
2026-05-06 14:17:12 +0300

    Add results saver for questions

---
+++ app/src/main/java/com/kernelpanic/vertblock/database/QuizResultEntity.kt
@@ -0,0 +1,13 @@
+package com.kernelpanic.vertblock.database
+
+@Entity(tableName = "quiz_results")
+data class QuizResultEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val question: String,
+    val correctAnswer: String,
+    val userAnswer: String,
+    val attempts: Int, // с какой попытки ответил (1, 2, 3, 4)
+    val category: String, // тема (music, science, ...)
+    val answeredAt: Long = System.currentTimeMillis()
+)
